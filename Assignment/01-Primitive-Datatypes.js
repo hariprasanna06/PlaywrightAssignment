3cb7b609727641c1eef10be790494466e1ea7767
hariprasanna06 <104847933+hariprasanna06@users.noreply.github.com>
2024-11-27 19:05:34 +0530

    Add files via upload

---
+++ Assignment/01-Primitive-Datatypes.js
@@ -0,0 +1,18 @@
+let firstName = "Ivan";
+console.log("1. The first name of the user "+firstName +". The Datatype is "+ typeof firstName);
+
+let companyName =null;
+console.log("2. The Value & Datatype is " +companyName+" "+ typeof companyName);
+
+let mobileNumber = 6745699086;
+console.log("4. The mobileNumber is "+ mobileNumber+". The Datatype is "+ typeof mobileNumber);
+
+let isAutomation= true;
+console.log("5. The Datatype is "+typeof isAutomation);
+    
+let hasPlaywright;
+console.log("6. The Datatype is "+typeof hasPlaywright);
+
+
+
+
